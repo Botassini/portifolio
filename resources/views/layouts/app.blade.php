7a1a02c59e539bd1647f29f9ad6426928b77a6c2
Samuel Botassini <samuelbotassini@gmail.com>
2024-12-30 20:20:26 -0300

    FIX

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     @stack('css')
-    @livewireStyles
 </head>
 
 <body class="font-sans antialiased">
@@ -92,8 +91,5 @@
 </script>
 
 @stack('js')
-@livewireScripts
-
-
 </body>
 </html>
